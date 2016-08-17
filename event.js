1788ae76f97e16d3a3f6b122412c8a5af6c3d963
Shi Chong <miracle_sc@163.com>
2016-08-17 22:14:52 +0800

    Create event.js

---
+++ event.js
@@ -0,0 +1,77 @@
+var EventUtil = {
+	addHandler : function(element, type, handler){
+		if (element.addEventListener) {
+			element.addEventListener(type, handler, false);
+		} else if (element.attachEvent) {
+			element.attachEvent("on" + type, handler);
+		} else {
+			element["on" + type] = handler;
+		}
+	},
+	removeHandler : function(element, type, handler){
+		if (element.removeEventListener) {
+			element.removeEventListener(type, handler, false);
+		} else if (element.detachEvent) {
+			element.detachEvent("on" + type, handler);
+		} else {
+			element["on" + type] = null;
+		}
+	},
+	getEvent : function(event){
+		return event || window.event;
+	},
+	getTarget : function(event){
+		return event.target || event.srcElement;
+	},
+	preventDefault : function(event){
+		if (event.preventDefault) {
+			event.preventDefault();
+		} else {
+			event.returnValue = false;
+		}
+	},
+	stopPropagation : function(event){
+		if (event.stopPropagation) {
+			event.stopPropagation();
+		} else {
+			event.cancelBubble = true;
+		}
+	},
+	getRelatedTarget : function(){				//相关元素信息 mouseover 和 mouseout 事件才有
+		if (event.relatedTarget) {
+			return event.relatedTarget;
+		} else if (event.toElement) {
+			return event.toElement;
+		} else if (event.fromElement) {
+			return event.fromElement;
+		} else {
+			return null;
+		}
+	},
+	getButton : function(){						//鼠标按钮哪个键被按下
+		if (document.implementation.hasFeature("mouseEvents", "2.0")) {
+			return event.button;
+		} else {
+			switch(event.button) {
+				case 0:
+				case 1:
+				case 3:
+				case 5:
+				case 7:
+					return 0;
+				case 2:
+				case 6:
+					return 2;
+				case 4:
+					return 1;
+			}
+		}
+	},
+	getCharCode : function(){					//字符编码
+		if (typeof event.charCode == "number") {
+			return event.charCode;
+		} else {
+			return event.keyCode;
+		}
+	}
+}
